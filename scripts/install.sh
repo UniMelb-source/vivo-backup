5f1277841ddc3ad87bf4981f0c59c5cc2c5b581d
Thomas Sullivan <tom@nsp-precise.vm.nsp.nectar.org.au>
2013-02-13 16:39:22 +1100

    - Update install script for backup

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,7 +6,7 @@ USER_NAME=rdr-unimelb
 USER_GROUP=rdr-unimelb
 USER_SHELL=/bin/false
 
-BIN_DIR=${USER_HOME}/bin
+INSTALL_DIR=${USER_HOME}/bin
 BACKUP_DIR=/var/lib/rdr-unimelb/backup
 
 echo "Adding user to system..."
@@ -14,14 +14,12 @@ groupadd ${USER_GROUP}
 useradd -m -d ${USER_HOME} -g ${USER_GROUP} -s ${USER_SHELL} ${USER_NAME}
 
 echo "Copying binaries to install location..."
-mkdir -p ${BIN_DIR}
-cp -rv dist/* ${BIN_DIR}
-chown -R ${USER_NAME}:${USER_GROUP} ${BIN_DIR}
+mkdir -p ${INSTALL_DIR}
+cp -v backup-directories ${INSTALL_DIR}
+cp -v backup-files ${INSTALL_DIR}
+cp -v backup.sh ${INSTALL_DIR}
+chown -R ${USER_NAME}:${USER_GROUP} ${INSTALL_DIR}
 
 echo "Installing crontab..."
 cp scripts/crontab ${CRON_DIR}/rdr-unimelb-backup
-sed -i -e "s#%%USER_NAME%%#${USER_NAME}#g" -e "s#%%BIN_DIR%%#${BIN_DIR}#g" ${CRON_DIR}/rdr-unimelb-backup
-
-echo "Creating log directory..."
-mkdir -p ${BACKUP_DIR}
-chown -R ${USER_NAME}:${USER_GROUP} ${BACKUP_DIR}
+sed -i -e "s#%%USER_NAME%%#${USER_NAME}#g" -e "s#%%INSTALL_DIR%%#${INSTALL_DIR}#g" ${CRON_DIR}/rdr-unimelb-backup
